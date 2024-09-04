1970ab5bce6a11870206a34fc451475a7da88424
sam <samliao1@auforce.com>
2024-09-04 15:55:13 +0800

    ✨ feat: 新增 CLI 安裝與移除批次流程腳本

---
+++ install.bat
@@ -0,0 +1,17 @@
+echo 【請輸入 .\install.bat】
+
+echo 【解除安裝全域套件】
+del .\nupkg\*.nupkg
+
+echo 【打包全域套件】
+dotnet pack
+
+echo 【解除安裝全域套件】
+dotnet tool uninstall -g fsharp-cli
+
+echo 【安裝全域套件】
+dotnet tool install --global --add-source .\nupkg fsharp-cli
+
+echo 【查看全域套件】
+dotnet tool list -g
+
