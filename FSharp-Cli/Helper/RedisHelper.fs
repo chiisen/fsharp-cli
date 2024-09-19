f9b6c7121c1913759cea8075ef8dff2389d45674
chiisen <chiisen@hotmail.com>
2024-09-20 05:54:56 +0800

    ✨ feat: 新增讀取 redis

---
+++ FSharp-Cli/Helper/RedisHelper.fs
@@ -0,0 +1,13 @@
+﻿module RedisHelper
+
+open StackExchange.Redis
+open System
+
+let readRedisKey (connectionString: string) (key: string) =
+    let connection = ConnectionMultiplexer.Connect(connectionString)
+    let db = connection.GetDatabase()
+    let value = db.StringGet(key)
+    if value.IsNullOrEmpty then
+        None
+    else
+        Some (value.ToString()) // 使用括号包裹
--- FSharp-Cli/Program.fs
+++ FSharp-Cli/Program.fs
@@ -2,6 +2,7 @@
 open Argu
 open RunColorPrint // 引用 RunColorPrint 模組
 open RunPrint // 引用 RunPrint 模組
+open RedisHelper // 引用 RedisHelper 模組
 
 // 定义命令行错误类型
 type CliError =
@@ -13,12 +14,14 @@ type CliError =
 type CmdArgs =
     | [<AltCommandLine("-p")>] Print of message: string
     | [<AltCommandLine("-a")>] Add of message: string
+    | [<AltCommandLine("-r")>] ReadRedis of key: string
 
     interface IArgParserTemplate with
         member this.Usage =
             match this with
             | Print _ -> "Print a message"
             | Add _ -> "新增一個命令。"
+            | ReadRedis _ -> "讀取 Redis 中指定的 key。"
 
 // 获取退出代码，根据结果返回相应的整数值
 let getExitCode result =
@@ -50,6 +53,13 @@ let main argv =
             0 // 返回合适的退出代码（表示成功）
         | p when p.Contains(Add) ->
             (RunColorPrint ConsoleColor.Red (p.GetResult(Add)) |> ignore)
+            0 // 返回合适的退出代码（表示成功）
+        | p when p.Contains(ReadRedis) ->
+            let key = p.GetResult(ReadRedis)
+            let connectionString = "127.0.0.1:6379" // 替換為你的 Redis 連接字串
+            match readRedisKey connectionString key with
+            | Some value -> printfn "Redis key [ %s ] 的值是: %s" key value
+            | None -> printfn "Redis key [ %s ] 不存在或沒有值" key
             0 // 返回合适的退出代码（表示成功）
         | p ->
             // 处理其他未被捕获的情况
