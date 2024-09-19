76cd61a7a8f2e78f36ac8787db18cb3239bafaf2
chiisen <chiisen@hotmail.com>
2024-09-20 05:25:59 +0800

    ✨ feat: 新增第二個參數

--- FSharp-Cli/Program.fs
+++ FSharp-Cli/Program.fs
@@ -10,25 +10,34 @@ type CliError =
 // 定义命令行参数类型
 type CmdArgs =
     | [<AltCommandLine("-p")>] Print of message: string
+    | [<AltCommandLine("-a")>] Add of message: string
 
     interface IArgParserTemplate with
         member this.Usage =
             match this with
             | Print _ -> "Print a message"
+            | Add _ -> "新增一個命令。"
 
 // 获取退出代码，根据结果返回相应的整数值
 let getExitCode result =
     match result with
     | Ok() -> 0
-    | Error err -> 
+    | Error err ->
         match err with
         | ArgumentsNotSpecified -> 1
         | _ -> 2 // 对于其他未指定的错误，返回一个默认的退出代码，例如2
 
-
 // 执行打印功能，将消息输出到控制台
 let runPrint print =
     printfn "%s" print
+    Ok()
+
+// 执行打印功能，将消息输出到控制台
+let runColorPrint color print =
+    let originalColor = Console.ForegroundColor
+    Console.ForegroundColor <- color
+    printfn "%s" print
+    Console.ForegroundColor <- originalColor
     Ok()
 
 [<EntryPoint>]
@@ -47,23 +56,22 @@ let main argv =
 
     let exitCode =
         match parser.ParseCommandLine argv with
-        | p when p.Contains(Print) -> 
-            (runPrint (p.GetResult(Print)) |> ignore) 
+        | p when p.Contains(Print) ->
+            (runPrint (p.GetResult(Print)) |> ignore)
             0 // 返回合适的退出代码（表示成功）
-        | p -> 
+        | p when p.Contains(Add) ->
+            (runColorPrint ConsoleColor.Red (p.GetResult(Add)) |> ignore)
+            0 // 返回合适的退出代码（表示成功）
+        | p ->
             // 处理其他未被捕获的情况
             printfn "未识别的参数: %A" p
             1 // 返回错误的退出代码
-
-
 
     let getExitCode code =
         if code = 0 then
             Ok() // 或者其他适当的处理
         else
             Error(CliError.SomeError("出错信息")) // 提供具体的错误信息
-
-
 
     printfn "按任何鍵繼續..."
     Console.ReadKey() |> ignore // 等待用户按下任何键
